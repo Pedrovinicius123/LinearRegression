b47fbb5678533b05e0e023d55d6bb49787ce86d1
Pedrovinicius123 <pvasx123@gmail.com>
2025-04-07 10:47:58 -0300

    Add tests to regression.py

--- Regression/regression.py
+++ Regression/regression.py
@@ -5,6 +5,7 @@ import random, time
 from alive_progress import alive_bar
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression
 
 def mae_loss(X, y):
     return np.mean(np.abs(X-y))
@@ -58,18 +59,31 @@ def regression(X, y, epochs=20, learning_rate=0.001):
 
 if __name__ == '__main__':
     inp = np.linspace(0, 10, num=300) + 0.0001
-    outer_factor = np.linspace(0, 1, num=300) + 0.0001
-    quadri_factor = np.linspace(1, 3, num=300) + 0.0001
 
     data = 3*inp + 10
-    data += np.random.uniform(-0.5, 0.5, size=300)
+    data += np.random.uniform(-15, 15, size=300)
+    
 
     X_train, X_test, y_train, y_test = train_test_split(inp, data, test_size=0.3, shuffle=True)
 
-    plt.scatter(X_train, y_train)
-    plt.scatter(X_test, y_test, color='red')
+    model_fitted = LinearRegression().fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+    
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+    axs[0].scatter(X_train, y_train)
+    axs[1].scatter(X_train, y_train)
+
+    axs[0].scatter(X_test, y_test, color='red')
+    axs[1].scatter(X_test, y_test, color='red')
+    
+    axs[0].set_title("MY MODEL")
+    axs[1].set_title('SKLEARN MODEL')
 
     f, ang, lin = regression(np.array(X_train), np.array(y_train), epochs=300, learning_rate=0.001)
-    plt.plot(np.array(X_test), f(X_test), color='green')
+    axs[0].plot(X_test, f(X_test), color='green')
+    axs[1].plot(X_test, model_fitted.predict(X_test.reshape(-1,1)), color='green')
+
+    print("MY MODEL: ", r2_score(f(X_test), y_test))
+    print("SKLEARN MODEL: ", r2_score(model_fitted.predict(X_test.reshape(-1,1)), y_test))
 
     plt.show()
